45ef5650edfd9691c48348ea2a0e73ffb5a7b606
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-08-19 12:56:59 +0100

    unstaticify the main class

--- src/musicplayer/MusicPlayer.java
+++ src/musicplayer/MusicPlayer.java
@@ -18,33 +18,6 @@ import musicplayer.utility.Log;
 public class MusicPlayer {
 	
 	public static void main(String[] args) throws IOException, ParseException {
-		new MusicPlayer().run();
-	}
-	
-	int current_view = 2;
-	public static final int VIEW_MINIFIED = 1; // The condensed music player view
-	public static final int VIEW_PLAYLIST = 2; // Viewing the current list of songs
-	public static final int VIEW_LIBRARY  = 3; // Viewing the list of albums & playlists
-
-	public boolean shuffle = false;
-	public boolean paused = false;
-	
-	public int loop = 0;
-	public static final int LOOP_NONE = 0; // Don't loop
-	public static final int LOOP_SONG = 1; // 
-	public static final int LOOP_LIST = 2; // Loop the playlist
-
-	public boolean pinned = false;
-	
-	// Variables for the current playlist
-	String playlist = "default";
-	int song_index = 0;
-	
-	ArrayList<String> current_playlist_song_list = null;
-	
-	G_SongControls controls = new G_SongControls();
-	
-	private void run() throws IOException, ParseException {
 		
 		// TODO: this is scuffed i need to refactor audio
 		AudioDevice d = new AudioDevice(AudioDevice.defaultDevice()); 
@@ -87,10 +60,33 @@ public class MusicPlayer {
 		}
 		
 		d.end();
-		
 	}
 	
-	public void set_current_playlist(String name) {
+	int current_view = 2;
+	public static final int VIEW_MINIFIED = 1; // The condensed music player view
+	public static final int VIEW_PLAYLIST = 2; // Viewing the current list of songs
+	public static final int VIEW_LIBRARY  = 3; // Viewing the list of albums & playlists
+
+	public boolean shuffle = false;
+	public boolean paused = false;
+	
+	public int loop = 0;
+	public static final int LOOP_NONE = 0; // Don't loop
+	public static final int LOOP_SONG = 1; // 
+	public static final int LOOP_LIST = 2; // Loop the playlist
+
+	public boolean pinned = false;
+	
+	// Variables for the current playlist
+	static String playlist = "default";
+	static int song_index = 0;
+	
+	static ArrayList<String> current_playlist_song_list = null;
+	
+	static G_SongControls controls = new G_SongControls();
+	
+	
+	static public void set_current_playlist(String name) {
 		playlist = name;
 		playlist_gui = new G_ScrollableList();
 		for (Song song : Library.getPlaylist(playlist).listSongs()) {
@@ -101,13 +97,13 @@ public class MusicPlayer {
 		}
 	}
 	
-	public void draw_library_view() {
+	static public void draw_library_view() {
 		
 		
 	}
 	
-	G_ScrollableList playlist_gui;
-	public void draw_playlist_view() {
+	static G_ScrollableList playlist_gui;
+	static public void draw_playlist_view() {
 		GraphicsAPI.color(0.15f, 0.25f, 0.5f, 1);
 		GraphicsAPI.rect(0, 0, GraphicsAPI.width(), 30, 0);
 		GraphicsAPI.color(1, 0.75f, 0.75f, 1);
@@ -118,7 +114,7 @@ public class MusicPlayer {
 		GraphicsAPI.text(GraphicsAPI.mouseX(), GraphicsAPI.mouseY(), 0, "Hello!");
 	}
 	
-	public void draw_minified_view() {
+	static public void draw_minified_view() {
 		
 	}
 	
