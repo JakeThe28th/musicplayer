b6a1ddf2f72eb142226f478f846c9e3ae24ae695
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-08-19 19:37:52 +0100

    bleh

---
+++ src/musicplayer/extensions/api/BasicImporter.java
@@ -0,0 +1,14 @@
+package musicplayer.extensions.api;
+
+/** Built-in extension that handles importing individual songs, playlists, and albums. */
+public class BasicImporter implements Extemsom {
+	
+	Extensions . register(new BasicImporter )
+	
+	etc TODO TODO
+	
+	menu.addOption
+	
+	enwfepeos
+
+}
