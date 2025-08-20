ee6bf935405c45d251a3b8980798a4d9d2825c3a
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-08-20 13:17:07 +0100

    so intellij doesn't freak out

--- src/musicplayer/extensions/api/BasicImporter.java
+++ src/musicplayer/extensions/api/BasicImporter.java
@@ -1,22 +1,22 @@
 package musicplayer.extensions.api;
 
 /** Built-in extension that handles importing individual songs, playlists, and albums. */
-public class BasicImporter implements Extemsom {
+public class BasicImporter{
 	
-	Extensions . register(new BasicImporter )
-	
-	etc TODO TODO
-	
-	menu.addOption
-	
-	enwfepeos
-	
-	register dragNDrop()
-	   -- If file.endswith ".zip", and view == library, add album/playlist
-	   -- if file.endswith ".wav" and view == playlist, popup add  song  dialog
-	   
-	maybe like
-	
-	MainProgram . register (G_Element custom screen ) ?? and switch to it for extra settings?
+//	Extensions . register(new BasicImporter )
+//	
+//	etc TODO TODO
+//	
+//	menu.addOption
+//	
+//	enwfepeos
+//	
+//	register dragNDrop()
+//	   -- If file.endswith ".zip", and view == library, add album/playlist
+//	   -- if file.endswith ".wav" and view == playlist, popup add  song  dialog
+//	   
+//	maybe like
+//	
+//	MainProgram . register (G_Element custom screen ) ?? and switch to it for extra settings?
 
 }
