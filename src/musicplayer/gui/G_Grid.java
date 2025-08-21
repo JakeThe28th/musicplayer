59f02d88ff7c863f3460e859682dae6cf7d28157
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-08-21 09:16:13 +0100

    Add minimum and maximum item sizes to the album grid

--- src/musicplayer/gui/G_Grid.java
+++ src/musicplayer/gui/G_Grid.java
@@ -13,6 +13,10 @@ public class G_Grid extends G_Element {
 		elements.add(element);
 	}
 	
+	public static final int MAX_ITEM_SIZE = 300; // Overrides TARGET_COLUMN_COUNT if necessary
+	public static final int MIN_ITEM_SIZE = 150;  // Overrides TARGET_COLUMN_COUNT if necessary
+	public static final int TARGET_COLUMN_COUNT = 3;
+
 	int columns = 3;
 	int item_size = 10;
 	
@@ -25,10 +29,29 @@ public class G_Grid extends G_Element {
 	@Override
 	public void layout(int left, int top, int right, int bottom) {
 
+		columns = TARGET_COLUMN_COUNT;
+		
 		int xx = left + left_margin;
 		int yy = top + top_margin;
-		item_size = ( (right-right_margin) - xx ) / columns;
+		int width = ( (right-right_margin) - xx );
 		
+		item_size = width / columns;
+		while (item_size > MAX_ITEM_SIZE) {
+			// item_size = MAX_ITEM_SIZE; <-- wouldn't fill the space properly
+			// so instead, find the smallest amount of columns that allow for
+			// an item size under the max
+			columns++;
+			item_size = width / columns;
+		}
+		
+		while (item_size - 1 < MIN_ITEM_SIZE) {
+			// item_size = MIN_ITEM_SIZE; <-- ALSO wouldn't fill the space properly
+			// so instead, find the largest amount of columns that allow for
+			// an item size over the minimum
+			columns--;
+			item_size = width / columns;
+		}
+				
 		int current_column = 0;
 		for (G_Element element : elements) {
 
