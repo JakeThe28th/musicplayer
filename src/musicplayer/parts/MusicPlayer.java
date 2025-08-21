f2497be883cfaf9639079d8df88af22668e20d80
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-08-21 21:51:32 +0100

    change title format to add album

--- src/musicplayer/parts/MusicPlayer.java
+++ src/musicplayer/parts/MusicPlayer.java
@@ -91,7 +91,7 @@ public class MusicPlayer {
 		
 		scroll_to_current();
 		
-		GraphicsAPI.title(current_song.name() + " - " + MainProgram.PROGRAM_TITLE);
+		GraphicsAPI.title(current_song.name() + " (" + current_song.uuid().album + ") - " + MainProgram.PROGRAM_TITLE);
 
 	}
 	
