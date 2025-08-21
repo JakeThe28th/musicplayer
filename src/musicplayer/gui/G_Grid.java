28cb4f7c44414754815e0b5b2b1fdb169fe18832
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-08-21 12:53:37 +0100

    division by 0 fix

--- src/musicplayer/gui/G_Grid.java
+++ src/musicplayer/gui/G_Grid.java
@@ -35,6 +35,8 @@ public class G_Grid extends G_Element {
 		int yy = top + top_margin;
 		int width = ( (right-right_margin) - xx );
 		
+		if (width == 0) width = 1;
+		
 		item_size = width / columns;
 		while (item_size > MAX_ITEM_SIZE) {
 			// item_size = MAX_ITEM_SIZE; <-- wouldn't fill the space properly
