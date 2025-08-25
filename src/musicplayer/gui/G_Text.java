eb5b3a0c75542a0edce390c747f05585a8f69d8e
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-08-25 16:22:41 +0100

    text vertical alignmnet

--- src/musicplayer/gui/G_Text.java
+++ src/musicplayer/gui/G_Text.java
@@ -36,7 +36,8 @@ public class G_Text extends G_Element {
 	public void layout(int left, int top, int right, int bottom) {
 		int xoffset = GUIUtility.getAlignmentOffset(left, right, width(), horizontal_align);
 		x = left + left_margin + xoffset;
-		y = top + top_margin;
+		int yoffset = GUIUtility.getAlignmentOffset(top, bottom, height(), vertical_align);
+		y = top + top_margin + yoffset;
 		
 		if (can_click) {
 			hover_rectangle = new Rectangle(left, top, right, bottom);
