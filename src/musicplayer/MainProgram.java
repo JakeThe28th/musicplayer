f4416253be89008598c0c5d19b468e8ca0ac49f5
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-09-12 11:12:40 +0100

    version

--- src/musicplayer/MainProgram.java
+++ src/musicplayer/MainProgram.java
@@ -32,7 +32,7 @@ public class MainProgram {
 	
 	public static final boolean SHOW_FPS = false;
 	
-	public static final String VERSION = "0.3.2a";
+	public static final String VERSION = "0.3.3a";
 	public static final String PROGRAM_NAME = "NOW PLAYING";
 	public static final String PROGRAM_TITLE = PROGRAM_NAME + " " + VERSION;
 	
