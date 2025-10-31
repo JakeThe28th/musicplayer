098e87f88a2898906fed8e7037a639d7fc60a4b7
JakeThe28th <42324960+JakeThe28th@users.noreply.github.com>
2025-10-31 22:58:11 +0000

    version bump

--- src/musicplayer/MainProgram.java
+++ src/musicplayer/MainProgram.java
@@ -34,7 +34,7 @@ public class MainProgram {
 	
 	public static boolean SHOW_FPS = Settings.show_fps();
 	
-	public static final String VERSION = "0.4.2a";
+	public static final String VERSION = "0.4.3a";
 	public static final String PROGRAM_NAME = "NOW PLAYING";
 	public static final String PROGRAM_TITLE = PROGRAM_NAME + " " + VERSION;
 	
